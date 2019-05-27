0d0a3c223eff861294be34021d5d50160df630c7
Philippe GASSMANN <philippe.gassmann@linkfluence.com>
2019-05-27 23:17:16 +0200

    broadcast type to send a message to multiple actors

--- src/broadcast.rs
+++ src/broadcast.rs
@@ -1,31 +1,61 @@
 use actix::prelude::*;
+use futures::{future, stream, Future, Stream};
 
-/// Broadcast a message to multiple actors
-struct Broadcast<M>
-where
-    M: Message<Result = ()>, // defines all possible bounds straight from the first struct
-    M: Clone,
-    M: 'static, {
-    //subscribers: Vec<Addr<Box<AsyncSubscriber<M>>>>,
+pub trait BroadcastableMessage: Message<Result = ()> + Clone + Send + 'static {}
+
+// auto impl
+impl<M> BroadcastableMessage for M where M: Message<Result = ()> + Clone + Send + 'static {}
+
+/// Broadcast a message to subscribers.
+///
+/// Fire & forget, if an error occurs it will be silently discarded
+pub struct Broadcast<M: BroadcastableMessage> {
+    subscribers: Vec<Recipient<M>>,
 }
 
-impl<M> Actor for Broadcast<M>
-where
-    M: Message<Result = ()>,
-    M: Clone,
-    M: 'static,
-{
+impl<M: BroadcastableMessage> Broadcast<M> {
+    pub fn new() -> Self {
+        Broadcast {
+            subscribers: Vec::new(),
+        }
+    }
+}
+
+impl<M: BroadcastableMessage> Actor for Broadcast<M> {
     type Context = Context<Self>;
 }
-impl<M> Handler<M> for Broadcast<M>
-where
-    M: Message<Result = ()>,
-    M: Clone,
-    M: 'static,
-{
+
+impl<M: BroadcastableMessage> Handler<M> for Broadcast<M> {
     type Result = ();
 
     fn handle(&mut self, msg: M, ctx: &mut Self::Context) -> Self::Result {
-        unimplemented!()
+        // Convert subscribers into a stream of future, resolves them in parallel using
+        // buffer_unordered
+        let futures: Vec<_> = self
+            .subscribers
+            .iter()
+            .map(|subscriber| {
+                subscriber
+                    .clone()
+                    .send(msg.clone())
+                    .map_err(|e| error!("Unable to deliver message {}", e))
+            })
+            .collect();
+        stream::iter_ok(futures)
+            .buffer_unordered(self.subscribers.len())
+            .fold((), |acc, x| future::ok(())) // consumer every element of the stream into nothing
+            .into_actor(self)
+            .spawn(ctx);
+    }
+}
+
+#[derive(Message)]
+pub struct RegisterRecipient<M: BroadcastableMessage>(pub Recipient<M>);
+
+impl<M: BroadcastableMessage> Handler<RegisterRecipient<M>> for Broadcast<M> {
+    type Result = ();
+
+    fn handle(&mut self, msg: RegisterRecipient<M>, ctx: &mut Self::Context) -> Self::Result {
+        self.subscribers.push(msg.0);
     }
 }
